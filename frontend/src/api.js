8ac761b52ba99f7676d0479da041534e1a5fbbf1
Manuel Grutsch <grutschm@gmail.com>
2024-07-23 16:37:42 +0200

    Store Authentication token after login and include in subsequent API request

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,6 +4,12 @@ const api = axios.create({
   baseURL: '/api',
 });
 
+// Set the Authorization header if token exists
+const token = localStorage.getItem('token');
+if (token) {
+  api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+}
+
 export const register = async (username, password) => {
   return await api.post('/register', { username, password });
 };
@@ -12,6 +18,7 @@ export const login = async (username, password) => {
   const response = await api.post('/login', { username, password });
   const token = response.data.token;
   if (token) {
+    localStorage.setItem('token', token);
     api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
   }
   return response;
@@ -23,4 +30,13 @@ export const createSession = async (name) => {
 
 export const getSessions = async () => {
   return await api.get('/sessions');
+};
+
+export const getGoogleAuthUrl = async () => {
+  const response = await api.get('/auth/google');
+  return response.data.url;
+};
+
+export const getCalendarEvents = async () => {
+  return await api.get('/calendar/events');
 };
